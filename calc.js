d93db36f3a099c7d005dfd9415cfbb3da0b08f71
Sarah-Z0 <85390494+Sarah-Z0@users.noreply.github.com>
2021-06-11 20:16:47 -0500

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
 function test(){
  var x = document.getElementById("b7").textContent;
- document.getElementById("tes").innerHTML = 77;
+ document.getElementById("tes").innerHTML = x;
 }
 /*const calculator = document.querySelector(‘.calculator’)
 const keys = calculator.querySelector(‘.calculator__keys’)
